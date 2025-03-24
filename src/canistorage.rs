23f44f01e29be70c30f4aec16e0f01c44908a59b
toshio <162767+toshio@users.noreply.github.com>
2025-03-25 00:26:16 +0900

    onwer information removed

--- src/canistorage.rs
+++ src/canistorage.rs
@@ -849,7 +849,7 @@ pub fn init_canistorage() -> Result<(), Error> {
     let file_info = get_file_info(&root);
     match file_info {
         Some(info) => {
-            error!(ERROR_ALREADY_INITIALIZED, format!("Canistorage already initialized by {}", info.creator))
+            error!(ERROR_ALREADY_INITIALIZED, "Already initialized")
         },
         None => {
             let owner = caller();
